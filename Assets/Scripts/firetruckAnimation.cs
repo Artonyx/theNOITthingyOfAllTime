f48a10f9b5ce94e69b04478a7d5acd633c2f278d
Artonyx <annamariadalakmanova@gmail.com>
2026-02-20 11:56:38 +0200

    pushing just in case cause im very gay

--- Assets/Scripts/firetruckAnimation.cs
+++ Assets/Scripts/firetruckAnimation.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class firetruckAnimation : MonoBehaviour
 {
+    private Animator animator;
+    private Vector3 lastPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        lastPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = transform.position - lastPos;
+        movement.Normalize();
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        lastPos = transform.position;
     }
 }
